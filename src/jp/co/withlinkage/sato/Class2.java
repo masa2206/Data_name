887196d1002bc42f654f0a6a9e3e77bc178f3fc0
ny2528 <naoky25@gmail.com>
2016-02-20 16:15:34 +0900

    add today's source

---
+++ src/jp/co/withlinkage/sato/Class2.java
@@ -0,0 +1,22 @@
+package jp.co.withlinkage.sato;
+
+public class Class2 implements If1{
+
+	protected int _pi;
+
+	private String name;
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
